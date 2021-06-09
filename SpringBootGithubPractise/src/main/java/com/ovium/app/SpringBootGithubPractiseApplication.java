87e20c69056c92f2af1488d84caa12e28097e148
Lovely Job <JobLovely@Gmail.com>
2021-06-09 18:07:37 -0500

    Second Change To Verify.

--- SpringBootGithubPractise/src/main/java/com/ovium/app/SpringBootGithubPractiseApplication.java
+++ SpringBootGithubPractise/src/main/java/com/ovium/app/SpringBootGithubPractiseApplication.java
@@ -8,7 +8,7 @@ public class SpringBootGithubPractiseApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootGithubPractiseApplication.class, args);
-		System.out.println("Hello Lovely!!!");
+		System.out.println("Hello Lovely!!!"+"My Second Change ");
 	}
 
 }
