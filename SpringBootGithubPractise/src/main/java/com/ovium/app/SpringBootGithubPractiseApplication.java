791ef960155f45aed1794ac36db83d319cdd1958
joblovely <51972557+joblovely@users.noreply.github.com>
2021-06-09 18:13:31 -0500

    Jerome Code Change

--- SpringBootGithubPractise/src/main/java/com/ovium/app/SpringBootGithubPractiseApplication.java
+++ SpringBootGithubPractise/src/main/java/com/ovium/app/SpringBootGithubPractiseApplication.java
@@ -9,6 +9,7 @@ public class SpringBootGithubPractiseApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootGithubPractiseApplication.class, args);
 		System.out.println("Hello Lovely!!!"+"My Second Change ");
+		System.out.println("Hello Lovely!!!"+"Jerome Code Change");
 	}
 
 }
