7cd42f668db341de2abef40f78f78516eba3f225
Lovely Job <JobLovely@Gmail.com>
2021-06-09 15:32:37 -0500

    First Commit On June 9 2021.

---
+++ SpringBootGithubPractise/src/main/java/com/ovium/app/SpringBootGithubPractiseApplication.java
@@ -0,0 +1,14 @@
+package com.ovium.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGithubPractiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGithubPractiseApplication.class, args);
+		System.out.println("Hello Lovely!!!");
+	}
+
+}
